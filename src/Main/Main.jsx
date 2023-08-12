c9fcaba1167d29cbd9705c6b66393226a5fa515f
tanzim-nahid <tanzimnahid6@gmail.com>
2023-08-12 23:45:27 +0600

    messaging section done

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Header/Banner";
 import NavBar from "../components/Header/NavBar";
+import Message from "../components/messaging/Message";
 
 const Main = () => {
   return (
     <div className="max-w-[1440px] ">
       <NavBar></NavBar>
       <Banner></Banner>
+      <Message></Message>
     </div>
   );
 };
---
+++ src/components/messaging/Message.jsx
@@ -0,0 +1,152 @@
+import "./message.css";
+import { BsArrowRightShort } from "react-icons/bs";
+const Message = () => {
+  return (
+    <div className="mt-[50px] w-full">
+      {/* Heading */}
+      <div className="text-center">
+        <h1 className="font-bold text-3xl">Messaging for all</h1>
+        <p className="text-gray-700 mt-2">
+          User generated content in real-time will have multiple touchpoints for
+          offshoring.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 md:grid-cols-3 message-css justify-center ">
+        <div className="w-[360px] lg:w-[380px] h-[320px] md:p-12 ">
+          <div >
+            <img
+              className="w-[48px] h-[48px] p-2 rounded-full bg-[#4775e8]"
+              src="./icon.png"
+              alt="icone"
+            />
+          </div>
+          <h1 className="text-black font-bold my-3">
+            Easier Work Organization
+          </h1>
+          <p className="text-[16px] text-gray-700">
+            Efficiently unleash cross-media information without cross-media
+            value. Quickly timely deliverables for real-time schemas.{" "}
+          </p>
+          <div className="text-primary flex gap-1 items-center my-3">
+            <span>Learn More </span>
+            <span>
+              <BsArrowRightShort className="mt-1" size={25}></BsArrowRightShort>
+            </span>
+          </div>
+        </div>
+        <div className="w-[360px] lg:w-[380px] h-[320px] md:p-12">
+          <div >
+            <img
+              className="w-[48px] h-[48px] p-2 rounded-full bg-[#4775e8]"
+              src="./icon.png"
+              alt="icone"
+            />
+          </div>
+          <h1 className="text-black font-bold my-3">
+            Easier Work Organization
+          </h1>
+          <p className="text-[16px] text-gray-700">
+            Efficiently unleash cross-media information without cross-media
+            value. Quickly timely deliverables for real-time schemas.{" "}
+          </p>
+          <div className="text-primary flex gap-1 items-center my-3">
+            <span>Learn More </span>
+            <span>
+              <BsArrowRightShort className="mt-1" size={25}></BsArrowRightShort>
+            </span>
+          </div>
+        </div>
+        <div className="w-[360px] lg:w-[380px] h-[320px] md:p-12">
+          <div >
+            <img
+              className="w-[48px] h-[48px] p-2 rounded-full bg-[#4775e8]"
+              src="./icon.png"
+              alt="icone"
+            />
+          </div>
+          <h1 className="text-black font-bold my-3">
+            Easier Work Organization
+          </h1>
+          <p className="text-[16px] text-gray-700">
+            Efficiently unleash cross-media information without cross-media
+            value. Quickly timely deliverables for real-time schemas.{" "}
+          </p>
+          <div className="text-primary flex gap-1 items-center my-3">
+            <span>Learn More </span>
+            <span>
+              <BsArrowRightShort className="mt-1" size={25}></BsArrowRightShort>
+            </span>
+          </div>
+        </div>
+        <div className="w-[360px] lg:w-[380px] h-[320px] p-12">
+          <div >
+            <img
+              className="w-[48px] h-[48px] p-2 rounded-full bg-[#4775e8]"
+              src="./icon.png"
+              alt="icone"
+            />
+          </div>
+          <h1 className="text-black font-bold my-3">
+            Easier Work Organization
+          </h1>
+          <p className="text-[16px] text-gray-700">
+            Efficiently unleash cross-media information without cross-media
+            value. Quickly timely deliverables for real-time schemas.{" "}
+          </p>
+          <div className="text-primary flex gap-1 items-center my-3">
+            <span>Learn More </span>
+            <span>
+              <BsArrowRightShort className="mt-1" size={25}></BsArrowRightShort>
+            </span>
+          </div>
+        </div>
+        <div className="w-[360px] lg:w-[380px] h-[320px] p-12">
+          <div >
+            <img
+              className="w-[48px] h-[48px] p-2 rounded-full bg-[#4775e8]"
+              src="./icon.png"
+              alt="icone"
+            />
+          </div>
+          <h1 className="text-black font-bold my-3">
+            Easier Work Organization
+          </h1>
+          <p className="text-[16px] text-gray-700">
+            Efficiently unleash cross-media information without cross-media
+            value. Quickly timely deliverables for real-time schemas.{" "}
+          </p>
+          <div className="text-primary flex gap-1 items-center my-3">
+            <span>Learn More </span>
+            <span>
+              <BsArrowRightShort className="mt-1" size={25}></BsArrowRightShort>
+            </span>
+          </div>
+        </div>
+        <div className="w-[360px] lg:w-[380px] h-[320px] p-12">
+          <div >
+            <img
+              className="w-[48px] h-[48px] p-2 rounded-full bg-[#4775e8]"
+              src="./icon.png"
+              alt="icone"
+            />
+          </div>
+          <h1 className="text-black font-bold my-3">
+            Easier Work Organization
+          </h1>
+          <p className="text-[16px] text-gray-700">
+            Efficiently unleash cross-media information without cross-media
+            value. Quickly timely deliverables for real-time schemas.{" "}
+          </p>
+          <div className="text-primary flex gap-1 items-center my-3">
+            <span>Learn More </span>
+            <span>
+              <BsArrowRightShort className="mt-1" size={25}></BsArrowRightShort>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
